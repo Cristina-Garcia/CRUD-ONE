c3eec304c09598ea1a78cf37c12e787a674579ef
Cristina <gre.cristina20@gmail.com>
2023-07-11 11:08:41 -0600

    promises

--- service/client-service.js
+++ service/client-service.js
@@ -24,19 +24,33 @@ const createNewLine = (nombre, email) => {
   line.innerHTML = content
   return line
 }
-
 const table = document.querySelector('[data-table]')
-const http = new XMLHttpRequest()
 
-//Abrir http(metodo,url)
-http.open('GET', 'http://localhost:3000/perfil')
-http.send()
+const listClients = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest()
 
-http.onload = () => {
-  const data = JSON.parse(http.response)
+    //Abrir http(metodo,url)
+    http.open('GET', 'http://localhost:3000/perfil')
+    http.send()
 
-  data.forEach((perfil) => {
-    const newLine = createNewLine(perfil.nombre, perfil.email)
-    table.appendChild(newLine)
+    http.onload = () => {
+      const response = JSON.parse(http.response)
+      if (http.status >= 400) {
+        reject(response)
+      } else {
+        resolve(response)
+      }
+    }
   })
+  return promise
 }
+
+listClients()
+  .then((data) => {
+    data.forEach((perfil) => {
+      const newLine = createNewLine(perfil.nombre, perfil.email)
+      table.appendChild(newLine)
+    })
+  })
+  .catch((error) => alert('hubo algun error'))
